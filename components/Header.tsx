cd2d0b5ebcf5583fb93b17059871bb4da545a164
kcabo <37576764+kcabo@users.noreply.github.com>
2025-03-30 21:47:49 +0900

    🐛 remove a tag inside Link tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,42 +1,36 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
-import logoImg from '@/public/miz-logo-landscape.svg';
+import logoImg from "@/public/miz-logo-landscape.svg";
 
 export function Header() {
   return (
-    <div className='sticky top-0 z-10 w-full mb-10 border-b border-gray-200 lg:mb-16 backdrop-blur-2xl'>
-      <div className='container flex items-center justify-between px-5 py-2 mx-auto max-w-7xl'>
-        <div className='flex '>
-          <Link href='/'>
-            <a className='w-24 pt-2'>
-              <Image src={logoImg} alt='MiZ' />
-            </a>
+    <div className="sticky top-0 z-10 w-full mb-10 border-b border-gray-200 lg:mb-16 backdrop-blur-2xl">
+      <div className="container flex items-center justify-between px-5 py-2 mx-auto max-w-7xl">
+        <div className="flex ">
+          <Link href="/" className="w-24 pt-2">
+            <Image src={logoImg} alt="MiZ" />
           </Link>
-          <nav className='items-center hidden pl-6 space-x-6 text-base md:flex md:ml-8 '>
-            <a
-              href='#feature'
-              className='font-medium leading-6 text-gray-600 hover:text-gray-900'
+          <nav className="items-center hidden pl-6 space-x-6 text-base md:flex md:ml-8 ">
+            <Link
+              href="/about"
+              className="font-medium leading-6 text-gray-600 hover:text-gray-900"
             >
-              特徴
-            </a>
-            <Link href='/about'>
-              <a className='font-medium leading-6 text-gray-600 hover:text-gray-900'>
-                運営者情報
-              </a>
+              運営者情報
             </Link>
-            <Link href='/terms'>
-              <a className='font-medium leading-6 text-gray-600 hover:text-gray-900'>
-                利用規約
-              </a>
+            <Link
+              href="/terms"
+              className="font-medium leading-6 text-gray-600 hover:text-gray-900"
+            >
+              利用規約
             </Link>
           </nav>
         </div>
 
-        <div className='ml-5 '>
+        <div className="ml-5 ">
           <a
-            href='https://lin.ee/LV3cuXP'
-            className='px-4 py-2.5 text-base font-medium leading-6 text-white whitespace-no-wrap rounded hover:shadow bg-sky-500 hover:shadow-sky-500/50'
+            href="https://lin.ee/LV3cuXP"
+            className="px-4 py-2.5 text-base font-medium leading-6 text-white whitespace-no-wrap rounded hover:shadow bg-sky-500 hover:shadow-sky-500/50"
           >
             友だち追加
           </a>
--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import { ArrowCircleRight } from 'iconsax-react';
+import Link from "next/link";
+import { ArrowCircleRight } from "iconsax-react";
 
 export function LinkButton({
   path,
@@ -9,11 +9,12 @@ export function LinkButton({
   caption: string;
 }) {
   return (
-    <Link href={path}>
-      <a className='flex items-center px-4 py-2 space-x-2 border rounded-md w-fit border-slate-200'>
-        <ArrowCircleRight size='24' color='#94a3b8' variant='Bulk' />
-        <div className='text-lg font-bold text-gray-700 '>{caption}</div>
-      </a>
+    <Link
+      href={path}
+      className="flex items-center px-4 py-2 space-x-2 border rounded-md w-fit border-slate-200"
+    >
+      <ArrowCircleRight size="24" color="#94a3b8" variant="Bulk" />
+      <div className="text-lg font-bold text-gray-700 ">{caption}</div>
     </Link>
   );
 }
